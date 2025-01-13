1898e66f2a2b26aa31a4f09b968ec4afa66309bd
MattAttackKachow <mathias.andersen06@gmail.com>
2025-01-13 12:52:08 +0100

    EntertainmentBar

--- LeSpaceGem/Assets/Scripts/PlayerController.cs
+++ LeSpaceGem/Assets/Scripts/PlayerController.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+
 
 public class PlayerController : MonoBehaviour
 {
-
+    public int ET;
+    public TextMeshProUGUI ETBar;
     public float speed;
+
     public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
-      
+        ET = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        ETBar.text = ET.ToString("0" + " Percent Entertained");
         if (Input.GetKeyDown(KeyCode.Space))
         {
 
