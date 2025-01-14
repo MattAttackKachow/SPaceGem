6469c735d2fc5d7c1b325bd544bfda7b22180ee7
MattAttackKachow <mathias.andersen06@gmail.com>
2025-01-14 15:13:45 +0100

    AllKeyComponents

--- LeSpaceGem/Assets/Scripts/MoveShip.cs
+++ LeSpaceGem/Assets/Scripts/MoveShip.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveShip : MonoBehaviour
 {
     public float speed;
-    Rigidbody2D rb;
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
