3858f016e6860d01930502454a30eea3c56d8025
MattAttackKachow <mathias.andersen06@gmail.com>
2025-01-14 15:04:16 +0100

    EEE

---
+++ LeSpaceGem/Assets/Scripts/MoveShip.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShip : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, speed);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -speed);
+
+        }
+    }
+}
